a1ef3048fe33ae8229fd138b739f963810bbc2c8
Adelwin Handoyo <adelwin.handoyo@prudential.com.sg>
2018-10-19 23:16:44 +0800

    initial commit, baseline code

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Adelwin Handoyo on 19/10/18.
+//  Copyright © 2018 Adelwin Handoyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
